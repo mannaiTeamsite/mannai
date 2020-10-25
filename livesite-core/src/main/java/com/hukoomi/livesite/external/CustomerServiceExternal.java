a463e27f370420804819699a5ba5820518f2d6f0
thavamani.sakthivel <thavamani.sakthivel@GFI-LPT326.gfi.in>
2020-10-25 12:56:09 +0530

    Renamed properties file for customer service

--- livesite-core/src/main/java/com/hukoomi/livesite/external/CustomerServiceExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/CustomerServiceExternal.java
@@ -11,16 +11,20 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.log4j.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 
 import com.hukoomi.utils.Constants;
 import com.hukoomi.utils.Postgre;
 import com.hukoomi.utils.VerifyRecaptcha;
+import com.ibm.faces.component.html.HtmlFileupload;
 import com.interwoven.livesite.runtime.RequestContext;
 import com.hukoomi.utils.VerifyRecaptcha;
 
@@ -34,56 +38,64 @@ public class CustomerServiceExternal {
 	private static final String ACTION_QUERY_TICKET = "queryticket";
 	private static final String ACTION_COMMENT = "comment";
 	private static final String ACTION_ID_TYPE = "idtype";
+	private static Connection connection;
+	static {
+		CustomerServiceExternal.connection = null;
+	}
 	
-	String action = "";
-	String eService = "";
-	String language = "";
-	String captcha = "";
+	
+	@SuppressWarnings("deprecation")
 	public Document getServices(RequestContext context) throws DocumentException, IOException {
-
-		Document Result = null;
+		String action = "";
+		String eService = "";
+		String language = "";
+		String captcha = "";
+		String ticketNumber = "";
+		HtmlFileupload file=null;
+		Document result = null;
 		logger.info("ReportIncident");
-		action = context.getParameterString("Action");
-		eService = context.getParameterString("eService");
-		language = context.getParameterString("language");
+		action = context.getParameterString("Action");		
 		if (!action.equals("")) {
 			switch (action) {
 			case ACTION_ESERVICE:
-				Result = getEServices();
+				result = getEServices();
 				break;
 			case ACTION_SERVICE:
-				Result = getServices();
+				eService = context.getParameterString("eService");
+				result = getServices(eService);
 				break;
 			case ACTION_SUBMIT_TICKET:
 				captcha = context.getParameterString("captcha");
-				Result = submitTicket();
+				file = (HtmlFileupload) context.getParameters().get("file");
+				result = submitTicket(captcha,file);
 				break;
 			case ACTION_QUERY_TICKET:
-				Result = queryTicket();
+				ticketNumber = context.getParameterString("ticketNumber");
+				result = queryTicket(ticketNumber);
 				break;
 			case ACTION_COMMENT:
-				Result = getComment();
+				eService = context.getParameterString("eService");
+				language = context.getParameterString("language");
+				result = getComment(eService,language);
 				break;
 			case ACTION_ID_TYPE:
-				Result = getIdType();
+				language = context.getParameterString("language");
+				result = getIdType(language);
 				break;
 			default:
 				break;
 			}
 		}
-		return Result;
+		return result;
 	}
 
-	private Document getIdType() throws UnsupportedEncodingException {
+	private Document getIdType(String language) throws UnsupportedEncodingException {
 		logger.debug("IncidentReport:getIdType:Entering");
-		Locale lang = getLocale();
-		logger.debug("IncidentReport:getComment:" + lang);
-		ResourceBundle bundle = ResourceBundle.getBundle("com.hukoomi.livesite.controller.ReportIncidentPortletResource", lang);
+		Locale lang = getLocale(language);
+		ResourceBundle bundle = ResourceBundle.getBundle("com.hukoomi.utils.ReportIncidentPortletResource", lang);
 		String validationMessage = bundle.getString("idType");
 		Document document = DocumentHelper.createDocument();
 		Element resultElement = document.addElement("Result");
-		
-		logger.debug("IncidentReport:getComment:" + validationMessage);
 		String[] arrOfStr = validationMessage.split(",", 2);
 		for(int i=0;i<arrOfStr.length;i++) {
 			String value = arrOfStr[i].split("!")[0];
@@ -103,28 +115,34 @@ public class CustomerServiceExternal {
 		return document;
 	}
 	
-	public String decodeToArabicString(String str) throws UnsupportedEncodingException{
+	public String decodeToArabicString(String str) {
 	      
-	      byte[] charset = str.getBytes("UTF-8");
-	      String result = new String(charset, "UTF-8");
-	      return result;
+	      byte[] charset = str.getBytes(StandardCharsets.UTF_8);
+	      return new String(charset, StandardCharsets.UTF_8);
 	   }
-	private Document getComment() throws DocumentException, UnsupportedEncodingException {
+	private Document getComment(String eService,String language) throws DocumentException {
 		logger.debug("IncidentReport:getComment:Entering");
-		Locale lang = getLocale();
+		Locale lang = getLocale(language);
 		logger.debug("IncidentReport:getComment:" + lang);
 		ResourceBundle bundle = ResourceBundle
-				.getBundle("com.hukoomi.livesite.controller.ReportIncidentPortletResource", lang);
-		String validationMessage = bundle.getString(eService);
+				.getBundle("com.hukoomi.utils.ReportIncidentPortletResource", lang);
+		String comments = bundle.getString(eService);
+		if(!comments.equals("") && !comments.equals(null) ) {
 		if(language.equals("ar")) {
-			validationMessage = decodeToArabicString(validationMessage);
+			comments = decodeToArabicString(comments);
 		}
-		logger.debug("IncidentReport:getComment:" + validationMessage);
-		validationMessage = "<comments>" + validationMessage + "</comments>";
-		return DocumentHelper.parseText(validationMessage);
+		logger.debug("IncidentReport:getComment:" + comments);
+		comments = "<comments>" + comments + "</comments>";
+		return DocumentHelper.parseText(comments);
+	}
+		else {
+			return null;
+		}
+		
+		
 	}
 
-	private Locale getLocale() {
+	private Locale getLocale(String language) {
 		switch (language) {
 		case "en":
 			return Locale.ENGLISH;
@@ -135,25 +153,39 @@ public class CustomerServiceExternal {
 		}
 	}
 
-	private Document queryTicket() {
-		return null;
+	private Document queryTicket(String ticketNumber) {
+		Document document = DocumentHelper.createDocument();
+		Element resultElement = document.addElement("Result");
+		Element eidElement = resultElement.addElement("EID");
+		eidElement.setText("0");
+		Element qidElement = resultElement.addElement("QID");
+		qidElement.setText("12312312311");
+		Element serviceElement = resultElement.addElement("Service");
+		serviceElement.setText("Resident Permits");
+		Element createdOnElement = resultElement.addElement("CreatedOn");
+		createdOnElement.setText("14-Oct-2020 10:01:43");
+		Element statusElement = resultElement.addElement("TicketStatus");
+		statusElement.setText("Open");
+		
+		return document;
 	}
 
-	private Document submitTicket() throws IOException, DocumentException {
-		logger.debug("submitTicket:Entering");
-		VerifyRecaptcha verify = new VerifyRecaptcha();
-	
-		return DocumentHelper.parseText( "<recaptcha>" + verify.verify(captcha) + "</recaptcha>");
+	private Document submitTicket( String captcha,HtmlFileupload file) throws IOException, DocumentException {
+		logger.debug("submitTicket:Entering");	
+		logger.debug("submitTicket:file:"+file.getValue());	
+		logger.debug("submitTicket:file:"+file.getSize());	
+		logger.debug("submitTicket:file:"+file.getFilename());	
+			
+		return DocumentHelper.parseText( "<recaptcha>" + VerifyRecaptcha.verify(captcha) + "</recaptcha>");
 	}
 
-	private Document getServices() throws UnsupportedEncodingException {
+	private Document getServices(String eService) throws IOException {
 		logger.debug("IncidentReportDAO:getServices:Entering");
-		Connection connection = null;
 		ResultSet resultSet = null;
 		Statement statement = null;
 		PreparedStatement preparedStatement = null;
 		StringBuilder builder = new StringBuilder();
-
+		connection = Postgre.getConnection();
 		Document document = DocumentHelper.createDocument();
 		Element resultElement = document.addElement("Result");
 
@@ -167,10 +199,10 @@ public class CustomerServiceExternal {
 		builder.append(") ORDER BY ESERVICEID ");
 		logger.debug("IncidentReportDAO:getServices:" + builder.toString());
 		try {
-			connection = Postgre.getConnection();
+			
 			if (null != connection) {
 				preparedStatement = connection.prepareStatement(builder.toString());
-				if (preparedStatement != null) {
+				
 					resultSet = preparedStatement.executeQuery();
 					while (resultSet.next()) {
 
@@ -182,12 +214,16 @@ public class CustomerServiceExternal {
 						Element serviceADecs = optionElement.addElement("ServiceADecs");
 						serviceADecs.setText(decodeToArabicString(resultSet.getString("SERVICEADESC")));
 					}
-				}
+					 OutputFormat format = OutputFormat.createPrettyPrint();
+			         XMLWriter writer;
+			         writer = new XMLWriter( System.out, format );
+			         writer.write( document );
 			}
 			logger.debug("IncidentReportDAO:getServices:result set");
-			logger.debug("IncidentReportDAO:getServices:Entering" + document);
+			logger.debug("IncidentReportDAO:getServices:document" + document.toString());
 		} catch (SQLException e) {
 			logger.error((Object) ("getConfiguration()" + e.getMessage()));
+			Postgre.releaseConnection(connection, statement, resultSet);
 			e.printStackTrace();
 			return document;
 		} finally {
@@ -197,17 +233,17 @@ public class CustomerServiceExternal {
 		return document;
 	}
 
-	private Document getEServices() throws UnsupportedEncodingException {
+	private Document getEServices() {
 		logger.debug("IncidentReportDAO:geteServices:Begin");
-		Connection connection = null;
 		ResultSet resultSet = null;
 		Statement statement = null;
+		CustomerServiceExternal.connection = Postgre.getConnection();
 		PreparedStatement preparedStatement = null;
 		StringBuilder builder = new StringBuilder();
 		List<Map<String, String>> resultList = null;
 		Document document = DocumentHelper.createDocument();
 		Element resultElement = document.addElement("Result");
-
+		
 		builder.append("SELECT E.\"ESERVICEID\", E.\"ESERVICEADESC\", E.\"ESERVICEEDESC\", E.\"MINISTRYID\" ");
 		builder.append("FROM public.\"ESERVICES\" E, public.\"INCIDENT_ESERVICE_MAPPING\" IEM ");
 		builder.append(
@@ -216,10 +252,10 @@ public class CustomerServiceExternal {
 
 		try {
 			logger.debug("IncidentReportDAO:geteServices:After builder");
-			connection = Postgre.getConnection();
-			if (null != connection) {
-				preparedStatement = connection.prepareStatement(builder.toString());
-				if (preparedStatement != null) {
+			
+			if (null != CustomerServiceExternal.connection) {
+				preparedStatement = CustomerServiceExternal.connection.prepareStatement(builder.toString());
+				
 					resultSet = preparedStatement.executeQuery();
 					while (resultSet.next()) {
 						Element optionElement = resultElement.addElement("Option");
@@ -229,7 +265,7 @@ public class CustomerServiceExternal {
 						eserviceEDecs.setText(resultSet.getString("ESERVICEEDESC"));
 						Element eserviceADecs = optionElement.addElement("EServiceADecs");
 						eserviceADecs.setText(decodeToArabicString(resultSet.getString("ESERVICEADESC")));
-					}
+					
 				}
 			}
 			logger.debug("IncidentReportDAO:geteServices:result set");
@@ -239,9 +275,9 @@ public class CustomerServiceExternal {
 			e.printStackTrace();
 			return document;
 		} finally {
-			Postgre.releaseConnection(connection, statement, resultSet);
+			Postgre.releaseConnection(CustomerServiceExternal.connection, statement, resultSet);
 		}
-		Postgre.releaseConnection(connection, statement, resultSet);
+		Postgre.releaseConnection(CustomerServiceExternal.connection, statement, resultSet);
 		return document;
 	}
 
