bb24db3395bd0ff1c3fc024d0a49c8756056fea8
Dipankar Borah <dipankar.borah@hayagreeva.co.in>
2020-11-24 10:09:50 +0530

    In getCuratedContent function initialization of root element of document object is changed

--- livesite-core/src/main/java/com/hukoomi/livesite/external/HukoomiCuratedExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/HukoomiCuratedExternal.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Document;
+import org.dom4j.Element;
 import org.dom4j.Node;
 
 import java.io.UnsupportedEncodingException;
@@ -15,38 +16,38 @@ import java.net.URLDecoder;
 import java.util.List;
 
 public class HukoomiCuratedExternal {
-/** Logger object to check the flow of the code.*/
-private final Logger logger =
-        Logger.getLogger(HukoomiCuratedExternal.class);
-/** Default query to fetch all solr content. */
-public static final String DEFAULT_QUERY = "*:*";
+    /** Logger object to check the flow of the code.*/
+    private final Logger logger =
+            Logger.getLogger(HukoomiCuratedExternal.class);
+    /** Default query to fetch all solr content. */
+    public static final String DEFAULT_QUERY = "*:*";
 
-/** This method will be called from Component
- * External for solr Content fetching.
- * @param context The parameter context object passed from Component.
- *
- * @return doc return the solr response document
- * generated from solr query.
- */
-public Document getLandingContent(final RequestContext context) {
-    Document document = DocumentHelper.createDocument();
-    String fieldQuery = "";
-    String fq = context.getParameterString(
-            "fieldQuery", "");
-    try {
-        fieldQuery = URLDecoder.decode(
-                fq, "UTF-8");
-        logger.debug("fieldQuery Query : " + fieldQuery);
-    } catch (UnsupportedEncodingException e) {
-        logger.warn("Unable to decode fieldQuery="
-                + fq, e);
+    /** This method will be called from Component
+     * External for solr Content fetching.
+     * @param context The parameter context object passed from Component.
+     *
+     * @return doc return the solr response document
+     * generated from solr query.
+     */
+    public Document getLandingContent(final RequestContext context) {
+        Document document = DocumentHelper.createDocument();
+        String fieldQuery = "";
+        String fq = context.getParameterString(
+                "fieldQuery", "");
+        try {
+            fieldQuery = URLDecoder.decode(
+                    fq, "UTF-8");
+            logger.debug("fieldQuery Query : " + fieldQuery);
+        } catch (UnsupportedEncodingException e) {
+            logger.warn("Unable to decode fieldQuery="
+                    + fq, e);
+        }
+        String curatedContent = context
+                .getParameterString("curated-content", "");
+        logger.debug("curated content : " + curatedContent);
+        return getCuratedContent(context,
+                document, curatedContent, fieldQuery);
     }
-    String curatedContent = context
-            .getParameterString("curated-content", "");
-    logger.debug("curated content : " + curatedContent);
-    return getCuratedContent(context,
-            document, curatedContent, fieldQuery);
-}
     /** This method will be called from Component
      * External for solr Content fetching.
      * @param node curated DCR replicant node instance.
@@ -58,8 +59,8 @@ public Document getLandingContent(final RequestContext context) {
      * field query.
      */
     public String getCuratedQuery(final String fq,
-              final Node node, final String city,
-                final String date) {
+                                  final Node node, final String city,
+                                  final String date) {
         String fieldQuery = fq;
         String curatedCategory = node
                 .selectSingleNode("ContentType").getText();
@@ -70,11 +71,11 @@ public Document getLandingContent(final RequestContext context) {
         }
         if (node.selectSingleNode("is-location-filter-required")
                 .getText().equals("yes") && StringUtils.isNotBlank(city)) {
-                fieldQuery += " AND " + city;
+            fieldQuery += " AND " + city;
         }
         if (node.selectSingleNode("is-date-filter-required")
                 .getText().equals("yes") && StringUtils.isNotBlank(date)) {
-                fieldQuery += " AND " + date;
+            fieldQuery += " AND " + date;
         }
         return fieldQuery;
     }
@@ -82,7 +83,7 @@ public Document getLandingContent(final RequestContext context) {
     /** This method will be called from Component
      * External for solr Content fetching.
      * @param context The parameter context object passed from Component.
-     * @param document The final content document to be returned.
+     * @param documentPar The final content document to be returned.
      * @param curatedContent curated DCR path passed as param to java.
      * @param fq solr field query.
      *
@@ -91,9 +92,13 @@ public Document getLandingContent(final RequestContext context) {
      */
     public Document getCuratedContent(
             final RequestContext context,
-            final Document document, final String curatedContent,
+            final Document documentPar, final String curatedContent,
             final String fq) {
+        Document document = DocumentHelper.createDocument();
+        Element docRoot = document.addElement("SolrResponse");
         CommonUtils commonUtils = new CommonUtils(context);
+        Document curDoc = null;
+        Element root = null;
         SolrQueryUtil squ = new SolrQueryUtil();
         SolrQueryBuilder sqb = new SolrQueryBuilder(context);
         String city = context.getParameterString(
@@ -105,50 +110,52 @@ public Document getLandingContent(final RequestContext context) {
                     .readDCR(curatedContent);
             String curatedXpath = context
                     .getParameterString(
-                    "curatedXpath",
-                    "/Root/CuratedContent/ContentType");
+                            "curatedXpath",
+                            "/Root/CuratedContent/ContentType");
             List<Node> categoryNodes =
                     curatedDoc.selectNodes(
                             curatedXpath);
             for (Node eleNode : categoryNodes) {
                 sqb.addFieldQuery(
                         getCuratedQuery(
-                    fq, eleNode, city, date));
+                                fq, eleNode, city, date));
 
                 String fields = context.getParameterString(
                         "fields", "");
                 logger.debug("fields : " + fields);
-            if (StringUtils.isNotBlank(fields)) {
-                logger.debug("fields : " + fields);
-                sqb.addFields(fields);
-            }
+                if (StringUtils.isNotBlank(fields)) {
+                    logger.debug("fields : " + fields);
+                    sqb.addFields(fields);
+                }
                 String query = sqb.build();
                 logger.debug("Landing Query : " + query);
-                Document curDoc = squ.doJsonQuery(
-                        query, "SolrResponse");
-                if (curDoc != null) {
+                curDoc = squ.doJsonQuery(
+                        query, "CuratedContent");
+                root = curDoc.getRootElement();
+                if (null != curDoc && null != eleNode
+                        && null != root) {
                     logger.debug("curDoc : " + curDoc);
-                    curDoc.getRootElement()
-                            .addElement("content-type")
+                    logger.debug("Content Type : "
+                            + eleNode.selectSingleNode("ContentType")
+                            .getText());
+
+                    root.addElement("content-type")
                             .addText(
                                     eleNode.selectSingleNode("ContentType")
                                             .getText());
-                    curDoc.getRootElement()
-                            .addElement("title")
+                    root.addElement("title")
                             .addText(
                                     eleNode.selectSingleNode("title")
                                             .getText());
-                    curDoc.getRootElement()
-                            .addElement("image")
+                    root.addElement("image")
                             .addText(
                                     eleNode.selectSingleNode("image")
                                             .getText());
-                    document.getRootElement().add(
-                            curDoc.getRootElement());
+                    docRoot.add(root);
                 }
             }
         }
         return document;
     }
-
 }
+    
