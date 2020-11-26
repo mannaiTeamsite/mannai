24a4602589eac3dd9c08b7a64cd79432c9a2e794
thavamani.sakthivel <thavamani.sakthivel@GFI-LPT326.gfi.in>
2020-11-26 13:56:32 +0530

    Maven build fixes

--- livesite-core/src/main/java/com/hukoomi/utils/Constants.java
+++ livesite-core/src/main/java/com/hukoomi/utils/Constants.java
@@ -1,14 +1,20 @@
 package com.hukoomi.utils;
-
-class Constants 
-{
-	private Constants() {
-	    throw new IllegalStateException("Utility class");
-	  }
-	public static final String ERROR_SYSTEM = "system_error";	
-	public static final String SUCCESS	= "success";
-	public static final String FALSE	= "false";
-	public static final String BLOCKED_SERVICES= "53001,53002,53003,53004,53005";
-	public static final String PROD_END_POINT	 = "https://motcsm.mirqab.gov.qa:13080/SM/7/ContactCenter.wsdl";
-	public static final String STAGING_END_POINT = "https://motcsm.mirqab.gov.qa:13080/SM/7/ContactCenter.wsdl";
+final class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+    /** error message. */
+    public static final String ERROR_SYSTEM = "system_error";
+    /** success message. */
+    public static final String SUCCESS = "success";
+    /** false message. */
+    public static final String FALSE = "false";
+    /** blocked services for customer services. */
+    public static final String BLOCKED_SERVICES = "53001,53002,53003,53004,53005";
+    /** production end point for customer service wsdl. */
+    public static final String PROD_END_POINT = 
+            "https://motcsm.mirqab.gov.qa:13080/SM/7/ContactCenter.wsdl";
+    /** staging end point for customer service wsdl. */
+    public static final String STAGING_END_POINT = 
+            "https://motcsm.mirqab.gov.qa:13080/SM/7/ContactCenter.wsdl";
 }
