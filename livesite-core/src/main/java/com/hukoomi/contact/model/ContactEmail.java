e5d7192943326c41e322a8b0b779b6f93b8b1252
thavamani.sakthivel <thavamani.sakthivel@GFI-LPT326.gfi.in>
2020-11-26 17:22:18 +0530

    fixes for maven build

--- livesite-core/src/main/java/com/hukoomi/contact/model/ContactEmail.java
+++ livesite-core/src/main/java/com/hukoomi/contact/model/ContactEmail.java
@@ -1,65 +1,87 @@
 package com.hukoomi.contact.model;
 
 import java.util.Locale;
-import java.util.ResourceBundle.Control;
 
 public class ContactEmail {
-	
-	private String senderName;
 
+    /** email senderName. */
+    private String senderName;
+    /** email sender emailid. */
 	private String senderEmail;
-
+	/** email text. */
 	private String emailText;
-	
+	/** email locale. */
 	private Locale locale;
-
-	/* private ContactHukoomiEmailSubject emailSubject; */
-	
+	/** email subject. */
 	private String emailSubject;
 
+	/**
+	 * @return
+	 */
 	public String getEmailSubject() {
 		return emailSubject;
 	}
 
+	/**
+	 * @param emailSubject
+	 */
 	public void setEmailSubject(String emailSubject) {
 		this.emailSubject = emailSubject;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getSenderName() {
 		return senderName;
 	}
 
+	/**
+	 * @param senderName
+	 */
 	public void setSenderName(String senderName) {
 		this.senderName = senderName;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getSenderEmail() {
 		return senderEmail;
 	}
 
+	/**
+	 * @param senderEmail
+	 */
 	public void setSenderEmail(String senderEmail) {
 		this.senderEmail = senderEmail;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getEmailText() {
 		return emailText;
 	}
 
+	/**
+	 * @param emailText
+	 */
 	public void setEmailText(String emailText) {
 		this.emailText = emailText;
 	}
+	/**
+	 * @return
+	 */
 	public Locale getLanguage() {
 		return locale;
 	}
+	/**
+	 * @param locale
+	 */
 	public void setLanguage(Locale locale) {
 		this.locale = locale;
 	}
-	/*
-	 * public ContactHukoomiEmailSubject getEmailSubject() { return emailSubject; }
-	 * 
-	 * public void setEmailSubject(ContactHukoomiEmailSubject emailSubject) {
-	 * this.emailSubject = emailSubject; }
-	 */
 
 
 }
