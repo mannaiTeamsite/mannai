57cdff507aafd6708560c35d15a5bc84cbe99ff1
thavamani.sakthivel <thavamani.sakthivel@GFI-LPT326.gfi.in>
2020-11-26 13:40:47 +0530

    Line has trailing and default constructor fixed

--- livesite-core/src/main/java/com/hukoomi/utils/Constants.java
+++ livesite-core/src/main/java/com/hukoomi/utils/Constants.java
@@ -1,15 +1,14 @@
 package com.hukoomi.utils;
 
-public class Constants 
+class Constants 
 {
-	
+	private Constants() {
+	    throw new IllegalStateException("Utility class");
+	  }
 	public static final String ERROR_SYSTEM = "system_error";	
 	public static final String SUCCESS	= "success";
 	public static final String FALSE	= "false";
-	//public static final String APPKEY= "appKey";	
 	public static final String BLOCKED_SERVICES= "53001,53002,53003,53004,53005";
 	public static final String PROD_END_POINT	 = "https://motcsm.mirqab.gov.qa:13080/SM/7/ContactCenter.wsdl";
 	public static final String STAGING_END_POINT = "https://motcsm.mirqab.gov.qa:13080/SM/7/ContactCenter.wsdl";
-	
-	
 }
