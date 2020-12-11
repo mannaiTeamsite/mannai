968004dc7f608285d4a452992deaa3bb4f73e9d4
j-saraiya <jatin.saraiya@hayagreeva.co.in>
2020-12-11 10:06:03 +0800

    Added PageScope Data for SEO

--- livesite-core/src/main/java/com/hukoomi/livesite/external/DetailExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/DetailExternal.java
@@ -4,12 +4,13 @@
 package com.hukoomi.livesite.external;
 
 import com.interwoven.livesite.runtime.RequestContext;
-//import com.interwoven.livesite.runtime.model.page.RuntimePage;
 import com.hukoomi.utils.CommonUtils;
+import com.interwoven.livesite.runtime.model.page.RuntimePage;
 import org.apache.log4j.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
-import org.dom4j.Node;
+
+import java.util.Map;
 
 public class DetailExternal {
 /** Logger object to check the flow of the code. */
@@ -30,18 +31,54 @@ public Document getContentDetail(final RequestContext context) {
     try {
         detailDocument = commonUtils.getDCRContent(context);
         if (detailDocument != null) {
-            /*processHtmlMetadata(context, detailDocument);*/
-            Node titleNode = detailDocument
-                    .selectSingleNode("/root/information/title");
-            if (titleNode != null) {
-                String title = titleNode.getStringValue();
-                context.getPageScopeData().put("record.title",
-                        title);
-                logger.info("Meta Title : " + title);
+            String title = commonUtils.getValueFromXML("/content/root/information/title", detailDocument);
+            context.getPageScopeData().put(RuntimePage.PAGESCOPE_TITLE, title);
+            logger.info("Set PageScope Title : " + title);
+            String locale = paramLocale.equals("ar") ? "ar_QA" :"en_US";
+            context.getPageScopeData().put("locale",locale);
+            logger.info("PageScope Locale : " + locale);
+            String description = commonUtils.getValueFromXML("/content/root/page-details/description", detailDocument);
+            logger.info("Set PageScope Meta Description to : " + description);
+            String keywords = commonUtils.getValueFromXML("/content/root/page-details/keywords", detailDocument);
+            logger.info("Set PageScope Meta Keywords to : " + keywords);
+            String contentCategory = commonUtils.getValueFromXML("/content/root/category", detailDocument);
+            String ogType = "";
+            if(!contentCategory.equals("Articles")){
+                ogType = "website";
+            } else {
+                ogType = contentCategory.toLowerCase();
+                logger.info("Setting PageScope Additional Metadata for Articles");
+                String articlePublishDate = commonUtils.getValueFromXML("/content/root/date", detailDocument);
+                context.getPageScopeData().put("article-published-time", articlePublishDate);
+                context.getPageScopeData().put("article-modified-time", commonUtils.getValueFromXML("/content/root/date", detailDocument));
+                logger.info("Set PageScope article-published-time / article-modified-time to : " + articlePublishDate);
+                context.getPageScopeData().put("article-tag", keywords);
+                logger.info("Set PageScope article-tag to : " + keywords);
             }
+            context.getPageScopeData().put("ogType", ogType);
+            logger.info("Set PageScope ogType to : " + ogType);
+
+            String imageValue = commonUtils.getValueFromXML("/content/root/information/image", detailDocument);
+            Map<String, Integer> imageDimensions = commonUtils.getImageDimensions(context.getFileDal().getRoot() + context.getFileDal().getSeparator() + imageValue);
+            if(!imageDimensions.isEmpty()){
+                int imageWidth = imageDimensions.containsKey("width") ? imageDimensions.get("width") : 0;
+                int imageHeight = imageDimensions.containsKey("height") ? imageDimensions.get("height") : 0;
+                context.getPageScopeData().put("image-width", imageWidth);
+                context.getPageScopeData().put("image-height", imageHeight);
+                logger.info("Set PageScope image dimensions as: " + imageWidth + " * " + imageHeight);
+            }
+            String currentPageLink = context.getPageLink(".");
+            String dcrName = context.getParameterString("record");
+            logger.info("Current Page Link " + currentPageLink);
+            String prettyURLforCurrentPage = commonUtils.getPrettyURLForPage(currentPageLink, paramLocale, dcrName);
+            context.getPageScopeData().put("href-lang-default", prettyURLforCurrentPage);
+            logger.info("Set PageScope href-lang-default as: " + prettyURLforCurrentPage);
+            context.getPageScopeData().put("href-lang-en", commonUtils.getPrettyURLForPage(currentPageLink, paramLocale, dcrName));
+            context.getPageScopeData().put("href-lang-ar", commonUtils.getPrettyURLForPage(currentPageLink, paramLocale, dcrName));
+            logger.info("Set PageScope href-lang attributes for Alternate Language");
         }
     } catch (Exception e) {
-        logger.error("Error fetching detail content for Blog in record "
+        logger.error("Error fetching detail content for record "
                 + context.getParameterString("record"), e);
     }
     return detailDocument;
