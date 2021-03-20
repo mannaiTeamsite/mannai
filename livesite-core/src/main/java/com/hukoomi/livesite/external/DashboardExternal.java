11b4447fe9a062fc172a6edff78c6c2adfb34f21
prasannakumar.prabhu <prasannakumar.prabhu@GFI-LPT251.gfi.in>
2021-03-20 10:29:00 +0300

    Adding new method to read session data

--- livesite-core/src/main/java/com/hukoomi/livesite/external/DashboardExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/DashboardExternal.java
@@ -3,13 +3,14 @@ package com.hukoomi.livesite.external;
 import java.io.IOException;
 import java.util.Properties;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.Element;
 import org.json.JSONObject;
 
 import com.hukoomi.utils.JWTTokenUtil;
@@ -103,9 +104,10 @@ public class DashboardExternal {
 
 	}
 
-	protected void doLogout(RequestContext context) throws ServletException, IOException {
+	public void doLogout(RequestContext context) throws IOException {
 
 		String url = properties.getProperty("logout");
+		LOGGER.info("Logout URL"+url);
 		// invalidate the session if exists
 		HttpServletRequest request = context.getRequest();
 		HttpSession session = request.getSession(false);
@@ -134,9 +136,103 @@ public class DashboardExternal {
 		}
 
 		url += "?relayURL=" + request.getRequestURL();
+		LOGGER.info("Logout URL"+url);
 		HttpServletResponse response = context.getResponse();
 		response.sendRedirect(url);
 
 	}
+	
+	public Document GetUserData(RequestContext context , Document doc) {
+		
+		 Element root = doc.getRootElement();
+		 HttpServletRequest request = context.getRequest();
+			HttpSession session = request.getSession(false);
+			LOGGER.info("Session:" + session);
+			LOGGER.info("Status : " + request.getSession().getAttribute("status"));
+			String status = (String) request.getSession().getAttribute("status");
+			String accessToken = null;
+			Cookie cookie = null;
+			Cookie[] cookies = null;
+			cookies = request.getCookies();
+			if (cookies != null) {
+				for (int i = 0; i < cookies.length; i++) {
+					cookie = cookies[i];
+					if (cookie.getName().equals("accessToken")) {
+						accessToken = cookie.getValue();
+					}
+				}
+			}
+			if (accessToken != null) {	
+				if (!status.equalsIgnoreCase("valid")) {
+					LOGGER.info("--------Dashboard External is called--------");
+					DashboardExternal dash = new DashboardExternal(context);
+					dash.dashboardServices(context, accessToken);
+
+					if (root != null && root.isRootElement()) {
+
+						Element userData = root.addElement("userData");
+
+						Element statusElement = userData.addElement("status");
+						statusElement.setText(request.getSession().getAttribute("status").toString());
+						Element uidElement = userData.addElement("uid");
+						uidElement.setText(request.getSession().getAttribute("uid").toString());
+						Element fnEnElement = userData.addElement("fnEn");
+						fnEnElement.setText(request.getSession().getAttribute("fnEn").toString());
+						Element fnArElement = userData.addElement("fnAr");
+						fnArElement.setText(request.getSession().getAttribute("fnAr").toString());
+						Element lnEnElement = userData.addElement("lnEn");
+						lnEnElement.setText(request.getSession().getAttribute("lnEn").toString());
+						Element lnArElement = userData.addElement("lnAr");
+						lnArElement.setText(request.getSession().getAttribute("lnAr").toString());
+						Element qIdElement = userData.addElement("QID");
+						qIdElement.setText(request.getSession().getAttribute("QID").toString());
+						Element eIdElement = userData.addElement("EID");
+						eIdElement.setText(request.getSession().getAttribute("EID").toString());
+						Element mobileElement = userData.addElement("mobile");
+						mobileElement.setText(request.getSession().getAttribute("mobile").toString());
+						Element emailElement = userData.addElement("email");
+						emailElement.setText(request.getSession().getAttribute("email").toString());
+						Element lstMdfyElement = userData.addElement("lstMdfy");
+						lstMdfyElement.setText(request.getSession().getAttribute("lstMdfy").toString());
+						Element roleElement = userData.addElement("role");
+						roleElement.setText(request.getSession().getAttribute("role").toString());
+					}
+				}
+			} else if (status.equalsIgnoreCase("valid") && root != null && root.isRootElement()) {
+
+				Element userData = root.addElement("userData");
+
+				Element statusElement = userData.addElement("status");
+				statusElement.setText(request.getSession().getAttribute("status").toString());
+				Element uidElement = userData.addElement("uid");
+				uidElement.setText(request.getSession().getAttribute("uid").toString());
+				Element fnEnElement = userData.addElement("fnEn");
+				fnEnElement.setText(request.getSession().getAttribute("fnEn").toString());
+				Element fnArElement = userData.addElement("fnAr");
+				fnArElement.setText(request.getSession().getAttribute("fnAr").toString());
+				Element lnEnElement = userData.addElement("lnEn");
+				lnEnElement.setText(request.getSession().getAttribute("lnEn").toString());
+				Element lnArElement = userData.addElement("lnAr");
+				lnArElement.setText(request.getSession().getAttribute("lnAr").toString());
+				Element qIdElement = userData.addElement("QID");
+				qIdElement.setText(request.getSession().getAttribute("QID").toString());
+				Element eIdElement = userData.addElement("EID");
+				eIdElement.setText(request.getSession().getAttribute("EID").toString());
+				Element mobileElement = userData.addElement("mobile");
+				mobileElement.setText(request.getSession().getAttribute("mobile").toString());
+				Element emailElement = userData.addElement("email");
+				emailElement.setText(request.getSession().getAttribute("email").toString());
+				Element lstMdfyElement = userData.addElement("lstMdfy");
+				lstMdfyElement.setText(request.getSession().getAttribute("lstMdfy").toString());
+				Element roleElement = userData.addElement("role");
+				roleElement.setText(request.getSession().getAttribute("role").toString());
+
+			}
+		return doc;
+	}
+	
+	
+	
+	
 
 }
