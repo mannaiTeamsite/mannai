dad6763111f0f4b7c745a2b7c7db2d18986af1ab
prasannakumar.prabhu <prasannakumar.prabhu@GFI-LPT251.gfi.in>
2021-03-21 12:01:08 +0300

    session changes

--- livesite-core/src/main/java/com/hukoomi/livesite/external/HukoomiExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/HukoomiExternal.java
@@ -71,12 +71,95 @@ public class HukoomiExternal {
 		}
 		logger.debug("Before calling : " + doc);
 
-		DashboardExternal dash = new DashboardExternal(context);
-		Document document = dash.GetUserData(context, doc);
+		 
+		 HttpServletRequest request = context.getRequest();
+			HttpSession session = request.getSession(false);
+			logger.info("Session:" + session);
+			logger.info("Status : " + request.getSession().getAttribute("status"));
+			String status = (String) request.getSession().getAttribute("status");
+		
+				if (!status.equalsIgnoreCase("valid")) {
+					String accessToken = null;
+					Cookie cookie = null;
+					Cookie[] cookies = null;
+					cookies = request.getCookies();
+					if (cookies != null) {
+						for (int i = 0; i < cookies.length; i++) {
+							cookie = cookies[i];
+							if (cookie.getName().equals("accessToken")) {
+								accessToken = cookie.getValue();
+							}
+						}
+					}
+					if (accessToken != null) {	
+						logger.info("--------Dashboard External is called--------");
+					DashboardExternal dash = new DashboardExternal(context);
+					dash.dashboardServices(context, accessToken);
+
+					if (root != null && root.isRootElement()) {
+
+						Element userData = root.addElement("userData");
+
+						Element statusElement = userData.addElement("status");
+						statusElement.setText(request.getSession().getAttribute("status").toString());
+						Element uidElement = userData.addElement("uid");
+						uidElement.setText(request.getSession().getAttribute("uid").toString());
+						Element fnEnElement = userData.addElement("fnEn");
+						fnEnElement.setText(request.getSession().getAttribute("fnEn").toString());
+						Element fnArElement = userData.addElement("fnAr");
+						fnArElement.setText(request.getSession().getAttribute("fnAr").toString());
+						Element lnEnElement = userData.addElement("lnEn");
+						lnEnElement.setText(request.getSession().getAttribute("lnEn").toString());
+						Element lnArElement = userData.addElement("lnAr");
+						lnArElement.setText(request.getSession().getAttribute("lnAr").toString());
+						Element qIdElement = userData.addElement("QID");
+						qIdElement.setText(request.getSession().getAttribute("QID").toString());
+						Element eIdElement = userData.addElement("EID");
+						eIdElement.setText(request.getSession().getAttribute("EID").toString());
+						Element mobileElement = userData.addElement("mobile");
+						mobileElement.setText(request.getSession().getAttribute("mobile").toString());
+						Element emailElement = userData.addElement("email");
+						emailElement.setText(request.getSession().getAttribute("email").toString());
+						Element lstMdfyElement = userData.addElement("lstMdfy");
+						lstMdfyElement.setText(request.getSession().getAttribute("lstMdfy").toString());
+						Element roleElement = userData.addElement("role");
+						roleElement.setText(request.getSession().getAttribute("role").toString());
+					}
+				}
+			} else if (status.equalsIgnoreCase("valid") && root != null && root.isRootElement()) {
+
+				Element userData = root.addElement("userData");
+
+				Element statusElement = userData.addElement("status");
+				statusElement.setText(request.getSession().getAttribute("status").toString());
+				Element uidElement = userData.addElement("uid");
+				uidElement.setText(request.getSession().getAttribute("uid").toString());
+				Element fnEnElement = userData.addElement("fnEn");
+				fnEnElement.setText(request.getSession().getAttribute("fnEn").toString());
+				Element fnArElement = userData.addElement("fnAr");
+				fnArElement.setText(request.getSession().getAttribute("fnAr").toString());
+				Element lnEnElement = userData.addElement("lnEn");
+				lnEnElement.setText(request.getSession().getAttribute("lnEn").toString());
+				Element lnArElement = userData.addElement("lnAr");
+				lnArElement.setText(request.getSession().getAttribute("lnAr").toString());
+				Element qIdElement = userData.addElement("QID");
+				qIdElement.setText(request.getSession().getAttribute("QID").toString());
+				Element eIdElement = userData.addElement("EID");
+				eIdElement.setText(request.getSession().getAttribute("EID").toString());
+				Element mobileElement = userData.addElement("mobile");
+				mobileElement.setText(request.getSession().getAttribute("mobile").toString());
+				Element emailElement = userData.addElement("email");
+				emailElement.setText(request.getSession().getAttribute("email").toString());
+				Element lstMdfyElement = userData.addElement("lstMdfy");
+				lstMdfyElement.setText(request.getSession().getAttribute("lstMdfy").toString());
+				Element roleElement = userData.addElement("role");
+				roleElement.setText(request.getSession().getAttribute("role").toString());
+
+			}
 
 		logger.info("Document" + doc.asXML());
 
-		return document;
+		return doc;
 
 	}
 
