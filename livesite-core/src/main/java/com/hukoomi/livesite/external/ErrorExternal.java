27302cc5ce137b3604a07c498b236f79ef6f4245
prasannakumar.prabhu <prasannakumar.prabhu@GFI-LPT251.gfi.in>
2021-04-07 14:15:36 +0300

    ESAPI changes for error page

--- livesite-core/src/main/java/com/hukoomi/livesite/external/ErrorExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/ErrorExternal.java
@@ -7,6 +7,10 @@ import org.apache.log4j.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.ValidationErrorList;
+
+import com.hukoomi.utils.ESAPIValidator;
 import com.hukoomi.utils.Postgre;
 import com.hukoomi.utils.RequestHeaderUtils;
 import com.interwoven.livesite.runtime.RequestContext;
@@ -31,9 +35,17 @@ public class ErrorExternal {
              if(status == 200) {
             	 status = 404;
              }
+             boolean insert = insertErrorResponse(context);
           	 Element errData = doc.addElement("errData");
         	 Element statusElement = errData.addElement("status");
-	         statusElement.setText(status.toString());
+        	 statusElement.setText(status.toString());
+        	 Element insertElement = errData.addElement("Inserted");
+        	 if(insert == true)
+        		 insertElement.setText("true");       		 
+        	 else
+        		 insertElement.setText("false");
+        		 
+	        
         
         logger.info("ErrorExternal : errorData ---- Ended");
 		return doc;		
@@ -41,16 +53,60 @@ public class ErrorExternal {
 	 private boolean insertErrorResponse(RequestContext context) {
          logger.info("ErrorExternal : insertErrorResponse");
          RequestHeaderUtils req = new RequestHeaderUtils(context);
-         Integer status = req.getStatus();
+         
+         final String LOCALE = "locale";
+         final String COMPONENT_TYPE = "componentType";
+         ValidationErrorList errorList = new ValidationErrorList();
+         
+         
+         String status = req.getStatus().toString();
+         
+         if (!ESAPIValidator.checkNull(status.toString())) {
+        	 status  = ESAPI.validator().getValidInput("status", status, ESAPIValidator.ALPHABET, 20, false, true, errorList);
+             if(!errorList.isEmpty()) {            
+                 logger.info(errorList.getError("status"));
+                 status = null;
+             }
+         }
+         
          String referer = req.getReferer();
+         if (!ESAPIValidator.checkNull(referer)) {
+        	 referer  = ESAPI.validator().getValidInput("referer", referer, ESAPIValidator.ALPHABET, 20, false, true, errorList);
+             if(!errorList.isEmpty()) {
+                 logger.info(errorList.getError("referer"));
+                 referer = null;
+                
+             }
+         }
+         
          String reqURL = req.getRequestURL();
-         String lang = req.getCookie("lang");
-         String compType = context.getParameterString("componentType");
+         if (!ESAPIValidator.checkNull(reqURL)) {
+        	 reqURL  = ESAPI.validator().getValidInput("reqURL", reqURL, ESAPIValidator.ALPHABET, 20, false, true, errorList);
+             if(!errorList.isEmpty()) {
+                 logger.info(errorList.getError("reqURL"));
+                 reqURL = null;
+                
+             }
+         }
+         
+         String lang = context.getParameterString(LOCALE);
+         if (!ESAPIValidator.checkNull(lang)) {
+        	 lang  = ESAPI.validator().getValidInput(LOCALE, lang, ESAPIValidator.ALPHABET, 20, false, true, errorList);
+             if(!errorList.isEmpty()) {
+                 logger.info(errorList.getError("lang"));
+                 lang = null;
+                
+             }
+         }
+        
+
+         
+         String compType = context.getParameterString(COMPONENT_TYPE);      
          logger.info("Componrnt Type:"+compType);
          
          boolean iserrorDataInserted = false;   
          
-         if(compType != null && compType.equalsIgnoreCase("Banner") && !referer.isBlank() && !reqURL.isBlank() && status != 200) {
+         if(compType != null && compType.equalsIgnoreCase("Banner") && referer != null && reqURL != null && req.getStatus() != 200 && status != null) {
         	  PreparedStatement errorprepareStatement = null;
               Connection connection = null;
               postgre = new Postgre(context);
@@ -68,10 +124,12 @@ public class ErrorExternal {
              errorprepareStatement.setString(1, reqURL);
              errorprepareStatement.setString(2, referer);
              errorprepareStatement.setString(3, lang);
-             errorprepareStatement.setLong(4, status);             
-             errorprepareStatement.executeUpdate();
+             errorprepareStatement.setString(4, status);             
+          
              
-             iserrorDataInserted = true;
+             if( errorprepareStatement.executeUpdate() == 1) {
+            	 iserrorDataInserted = true;
+             }
              logger.info("ErrorExternal : insertTable--End");   	
              
 		} catch (SQLException e) {
