434263bbb5b9e51f431fb3451b360b0be0c83bb2
Arbaj Shaikh <arbaj102@gmail.com>
2021-05-10 11:24:58 +0530

    Newsletter HTML template path added

--- livesite-core/src/main/java/com/hukoomi/livesite/external/NewsletterPhpExternal.java
+++ livesite-core/src/main/java/com/hukoomi/livesite/external/NewsletterPhpExternal.java
@@ -1,6 +1,9 @@
 package com.hukoomi.livesite.external;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,6 +28,7 @@ import com.hukoomi.utils.Postgre;
 import com.hukoomi.utils.PropertiesFileReader;
 import com.hukoomi.utils.ValidationUtils;
 import com.hukoomi.utils.XssUtils;
+import com.interwoven.livesite.file.FileDal;
 import com.interwoven.livesite.runtime.RequestContext;
 
 /**
@@ -93,6 +97,9 @@ public class NewsletterPhpExternal {
 
     /** character set Constant */
     private static final String CHAR_SET = "UTF-8";
+
+    /** Initialising the filepath for Properties file inside WorkArea. */
+    private static final String NEWSLETTER_TEMPLATE_PATH = "/iw/config/newsletter-html-templates/";
 
     /** MySql Object variable. */
     MySql mysql = null;
@@ -534,137 +541,20 @@ public class NewsletterPhpExternal {
             msg.setRecipient(Message.RecipientType.TO,
                     new InternetAddress(to));
 
-            subject = "Test Mail";
+            // Mail Subject
+            Properties propertiesFile = loadProperties(context,
+                    "phplist.properties");
+            subject = propertiesFile
+                    .getProperty("messageSubject_" + pageLanguage);
 
-            String message = "<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n"
-                    + "<head>\r\n" + "    <title></title>\r\n"
-                    + "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\r\n"
-                    + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n"
-                    + "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\r\n"
-                    + "    <style type=\"text/css\">\r\n"
-                    + "        @media screen {\r\n"
-                    + "            @font-face {\r\n"
-                    + "                font-family: 'Lato';\r\n"
-                    + "                font-style: normal;\r\n"
-                    + "                font-weight: 400;\r\n"
-                    + "                src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');\r\n"
-                    + "            }\r\n" + "\r\n"
-                    + "            @font-face {\r\n"
-                    + "                font-family: 'Lato';\r\n"
-                    + "                font-style: normal;\r\n"
-                    + "                font-weight: 700;\r\n"
-                    + "                src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');\r\n"
-                    + "            }\r\n" + "\r\n"
-                    + "            @font-face {\r\n"
-                    + "                font-family: 'Lato';\r\n"
-                    + "                font-style: italic;\r\n"
-                    + "                font-weight: 400;\r\n"
-                    + "                src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');\r\n"
-                    + "            }\r\n" + "\r\n"
-                    + "            @font-face {\r\n"
-                    + "                font-family: 'Lato';\r\n"
-                    + "                font-style: italic;\r\n"
-                    + "                font-weight: 700;\r\n"
-                    + "                src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');\r\n"
-                    + "            }\r\n" + "        }\r\n" + "\r\n"
-                    + "        /* CLIENT-SPECIFIC STYLES */\r\n"
-                    + "        body,\r\n" + "        table,\r\n"
-                    + "        td,\r\n" + "        a {\r\n"
-                    + "            -webkit-text-size-adjust: 100%;\r\n"
-                    + "            -ms-text-size-adjust: 100%;\r\n"
-                    + "        }\r\n" + "\r\n" + "        table,\r\n"
-                    + "        td {\r\n"
-                    + "            mso-table-lspace: 0pt;\r\n"
-                    + "            mso-table-rspace: 0pt;\r\n"
-                    + "        }\r\n" + "\r\n" + "        img {\r\n"
-                    + "            -ms-interpolation-mode: bicubic;\r\n"
-                    + "        }\r\n" + "\r\n"
-                    + "        /* RESET STYLES */\r\n"
-                    + "        img {\r\n" + "            border: 0;\r\n"
-                    + "            height: auto;\r\n"
-                    + "            line-height: 100%;\r\n"
-                    + "            outline: none;\r\n"
-                    + "            text-decoration: none;\r\n"
-                    + "        }\r\n" + "\r\n" + "        table {\r\n"
-                    + "            border-collapse: collapse !important;\r\n"
-                    + "        }\r\n" + "\r\n" + "        body {\r\n"
-                    + "            height: 100% !important;\r\n"
-                    + "            margin: 0 !important;\r\n"
-                    + "            padding: 0 !important;\r\n"
-                    + "            width: 100% !important;\r\n"
-                    + "        }\r\n" + "\r\n"
-                    + "        /* iOS BLUE LINKS */\r\n"
-                    + "        a[x-apple-data-detectors] {\r\n"
-                    + "            color: inherit !important;\r\n"
-                    + "            text-decoration: none !important;\r\n"
-                    + "            font-size: inherit !important;\r\n"
-                    + "            font-family: inherit !important;\r\n"
-                    + "            font-weight: inherit !important;\r\n"
-                    + "            line-height: inherit !important;\r\n"
-                    + "        }\r\n" + "\r\n"
-                    + "        /* MOBILE STYLES */\r\n"
-                    + "        @media screen and (max-width:600px) {\r\n"
-                    + "            h1 {\r\n"
-                    + "                font-size: 32px !important;\r\n"
-                    + "                line-height: 32px !important;\r\n"
-                    + "            }\r\n" + "        }\r\n" + "\r\n"
-                    + "        /* ANDROID CENTER FIX */\r\n"
-                    + "        div[style*=\"margin: 16px 0;\"] {\r\n"
-                    + "            margin: 0 !important;\r\n"
-                    + "        }\r\n" + "    </style>\r\n" + "</head>\r\n"
-                    + "\r\n"
-                    + "<body style=\"background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;\">\r\n"
-                    + "    <!-- HIDDEN PREHEADER TEXT -->\r\n"
-                    + "    <div style=\"display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;\"> We're thrilled to have you here! Get ready to dive into your new account. </div>\r\n"
-                    + "    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\r\n"
-                    + "        <!-- LOGO -->\r\n" + "        <tr>\r\n"
-                    + "            <td bgcolor=\"#FFA73B\" align=\"center\">\r\n"
-                    + "                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\r\n"
-                    + "                    <tr>\r\n"
-                    + "                        <td align=\"center\" valign=\"top\" style=\"padding: 40px 10px 40px 10px;\"> </td>\r\n"
-                    + "                    </tr>\r\n"
-                    + "                </table>\r\n"
-                    + "            </td>\r\n" + "        </tr>\r\n"
-                    + "        <tr>\r\n"
-                    + "            <td bgcolor=\"#FFA73B\" align=\"center\" style=\"padding: 0px 10px 0px 10px;\">\r\n"
-                    + "                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\r\n"
-                    + "                    <tr>\r\n"
-                    + "                        <td bgcolor=\"#ffffff\" align=\"center\" valign=\"top\" style=\"padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;\">\r\n"
-                    + "                            <h1 style=\"font-size: 48px; font-weight: 400; margin: 2;\">Welcome!</h1> <img src=\" https://img.icons8.com/clouds/100/000000/handshake.png\" width=\"125\" height=\"120\" style=\"display: block; border: 0px;\" />\r\n"
-                    + "                        </td>\r\n"
-                    + "                    </tr>\r\n"
-                    + "                </table>\r\n"
-                    + "            </td>\r\n" + "        </tr>\r\n"
-                    + "        <tr>\r\n"
-                    + "            <td bgcolor=\"#f4f4f4\" align=\"center\" style=\"padding: 0px 10px 0px 10px;\">\r\n"
-                    + "                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\r\n"
-                    + "                    <tr>\r\n"
-                    + "                        <td bgcolor=\"#ffffff\" align=\"left\" style=\"padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;\">\r\n"
-                    + "                            <p style=\"margin: 0;\">Thank you for subscribing to Hukoommi Newsletter. First, you need to confirm your subscription. Just press the button below.</p>\r\n"
-                    + "                        </td>\r\n"
-                    + "                    </tr>\r\n"
-                    + "                    <tr>\r\n"
-                    + "                        <td bgcolor=\"#ffffff\" align=\"left\">\r\n"
-                    + "                            <table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\r\n"
-                    + "                                <tr>\r\n"
-                    + "                                    <td bgcolor=\"#ffffff\" align=\"center\" style=\"padding: 20px 30px 60px 30px;\">\r\n"
-                    + "                                        <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\r\n"
-                    + "                                            <tr>\r\n"
-                    + "                                                <td align=\"center\" style=\"border-radius: 3px;\" bgcolor=\"#FFA73B\"><a href=\"https://dev.hukoomi.gov.qa/confirmation?token="
-                    + confirmationToken
-                    + "&lang=" + pageLanguage
-                    + "\" target=\"_blank\" style=\"font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;\">Confirm Subscription</a></td>\r\n"
-                    + "                                            </tr>\r\n"
-                    + "                                        </table>\r\n"
-                    + "                                    </td>\r\n"
-                    + "                                </tr>\r\n"
-                    + "                            </table>\r\n"
-                    + "                        </td>\r\n"
-                    + "                    </tr> \r\n"
-                    + "                </table>\r\n"
-                    + "            </td>\r\n" + "        </tr>\r\n"
-                    + "    </table>\r\n" + "</body>\r\n" + "\r\n"
-                    + "</html>";
+            String messageHtmlName = "newsletter-confirmation-mail-"
+                    + pageLanguage + ".html";
+            String message = getHtmlFile(messageHtmlName, context);
+            message = message.replace("<token>", confirmationToken)
+                    .replace("<lang>", pageLanguage);
+
+            logger.info("Confirmation Mail HTML :" + message);
+
 
             if (pageLanguage.equals("ar")) {
                 msg.setSubject(subject, CHAR_SET);
@@ -872,6 +762,39 @@ public class NewsletterPhpExternal {
                 context, propertyFile);
         return propertyFileReader.getPropertiesFile();
 
+    }
+
+    /**
+     * This method will be used to load the configuration properties.
+     *
+     * @param context
+     *                The parameter context object passed from Component.
+     * @throws IOException
+     * @throws MalformedURLException
+     *
+     */
+    private static String getHtmlFile(String htmlFileName,
+            RequestContext context) {
+        logger.info("NewsletterPhp External : getHtmlFile");
+        logger.info("Loading HTML File from Request Context.");
+        StringBuilder contentBuilder = new StringBuilder();
+
+        FileDal fileDal = context.getFileDal();
+        String root = fileDal.getRoot();
+        String fileName = root + NEWSLETTER_TEMPLATE_PATH + htmlFileName;
+
+        try (BufferedReader in = new BufferedReader(
+                new FileReader(fileName))) {
+
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+            }
+        } catch (IOException e) {
+            logger.error("Exception in getHtmlFile", e);
+        }
+        logger.info("Finish Loading Properties File.");
+        return contentBuilder.toString();
     }
 
 }
